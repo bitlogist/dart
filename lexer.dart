278dfeebd8cee4080b6c3da89aa0b4c605c8bb47
Bit Logist <thebitlogist@gmail.com>
2023-07-10 02:13:44 +0000

    modified lexer

--- lexer.dart
+++ lexer.dart
@@ -7,16 +7,23 @@ class Lexer {
   List<Token> tokens = [];
   bool end = false;
 
+  List<String> keywords = ['return'];
+  List<String> declarations = ['var', 'const'];
+  List<String> conditionals = ['if', 'while'];
+  String symbols = '<=>+-';
+  String assignment = '<-';
+  List<String> ignore = [' ', '\n'];
+
   Lexer(this.code) {
     this.char = code[index];
   }
 
   void moveCursor() {
-    index++;
-    if (index < code.length) {
-      this.char = code[index];
+    this.index++;
+    if (index < this.code.length) {
+      this.char = this.code[this.index];
     } else {
-      end = true;
+      this.end = true;
     }
   }
 
@@ -25,24 +32,75 @@ class Lexer {
   }
 
   bool isNumeric(String letter) {
-    return '0123456789'.contains(letter);
+    return '.0123456789'.contains(letter);
+  }
+
+  bool isSymbol(String letter) {
+    return this.symbols.contains(letter);
   }
 
   String getWord() {
     String word = '';
-    while (!end && isValidLetter(this.char)) {
+    while (!this.end && isValidLetter(this.char)) {
       word += this.char;
       this.moveCursor();
     }
     return word;
   }
 
+  String getNumber() {
+    String num = '';
+    while (!this.end && isNumeric(this.char)) {
+      num += this.char;
+      this.moveCursor();
+    }
+    return num;
+  }
+
+  String getSymbols() {
+    String symbols = '';
+    while (!this.end && isSymbol(this.char)) {
+      symbols += this.char;
+      this.moveCursor();
+    }
+    return symbols;
+  }
+
   List<Token> generateTokens() {
-    while (!end) {
+    while (!this.end) {
       if (isValidLetter(this.char)) {
         String word = getWord();
-        this.tokens.add(new Call(word)); // every word is a function call
+        if (this.keywords.contains(word)) this.tokens.add(new Keyword(word));
+        else if (this.declarations.contains(word)) this.tokens.add(new Declaration(word));
+        else if (this.conditionals.contains(word)) this.tokens.add(new Conditional(word));
+        else this.tokens.add(new Reference(word));
+
+        this.moveCursor();
+        continue;
+      } 
+      if (isNumeric(this.char) || this.char == '+' || this.char == '-') {
+        String firstDigit = this.char; // could be digit or sign
+        this.moveCursor();
+        String num = firstDigit + getNumber();
+        if (num.contains('.')) this.tokens.add(new Double(num));
+        else this.tokens.add(new Integer(num));
+
+        this.moveCursor();
+        continue;
+      } 
+      if (isSymbol(this.char)) {
+        String symbols = this.getSymbols();
+        if (symbols == this.assignment) this.tokens.add(new Assignment(symbols));
+        else this.tokens.add(new Operator(symbols));
+
+        this.moveCursor();
+        continue;
+      } 
+      if (ignore.contains(this.char)) {
+        this.moveCursor();
+        continue;
       }
+      throw new Exception('invalid character');
     }
     return this.tokens;
   }
--- tokens.dart
+++ tokens.dart
@@ -4,16 +4,48 @@ class Token {
   Token(this.value);
 }
 
+class Keyword extends Token {
+  Keyword(value) : super(value);
+}
+
+class Declaration extends Token {
+  Declaration(value) : super(value);
+}
+
+class Assignment extends Token {
+  Assignment(value) : super(value);
+}
+
+class Conditional extends Token {
+  Conditional(value) : super(value);
+}
+
+class Operator extends Token {
+  Operator(value) : super(value);
+}
+
 class Call extends Token {
   Call(value) : super(value);
+}
+
+class Reference extends Token {
+  Reference(value) : super(value);
 }
 
 class Integer extends Token {
   Integer(value) : super(value);
 }
 
+class Double extends Token {
+  Double(value) : super(value);
+}
+
 class Text extends Token {
   Text(value) : super(value);
+}
+
+class End extends Token {
+  End(value) : super(value);
 }
 
 void printTokens(List<Token> tokens) {
